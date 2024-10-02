ebcdf2f80c0f49730232180a1defc8c3dfe828ca
Florin Chelaru <florin.chelaru@gmail.com>
2024-10-02 19:05:50 +0300

    create human with swinging arms

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-import BlasterScene from "./BlasterScene";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import HumanScene from "./CubeScene";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -15,17 +16,30 @@ const camera = new THREE.PerspectiveCamera(
   0.1, // near
   100 // far
 );
+camera.position.set(0, 0, 5);
+// camera.lookAt(0,0,0);
 
-const scene = new BlasterScene();
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(0, 5, 5);
+// light.target.position.set(0, 0, 0);
+camera.add(light);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+const scene = new HumanScene();
 scene.initialize();
+scene.add(camera);
 
-renderer.render(scene, camera);
 
-function tick() {
-//   scene.update();
+
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  scene.update();
+  
   renderer.render(scene, camera);
-  requestAnimationFrame(tick);
 }
 
-tick();
+animate();
